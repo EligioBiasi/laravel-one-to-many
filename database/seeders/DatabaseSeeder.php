3b73b534cf783fb1e1ab0e3a85e2b68dba6a4327
Eligio Biasi <ex_98@live.it>
2023-08-29 16:35:29 +0200

    added seeder to dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeSeeder::class,
             ProjectSeeder::class,
+            UserSeeder::class,
+            UserDetailSeeder::class,
         ]);
     }
 }
