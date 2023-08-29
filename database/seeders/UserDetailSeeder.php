731cf4e86df147fa19b6607a92f8aa4171e91fd2
Eligio Biasi <ex_98@live.it>
2023-08-29 16:33:53 +0200

    created user detail seeder

---
+++ database/seeders/UserDetailSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class UserDetailSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $users = User::all();
+    }
+}
