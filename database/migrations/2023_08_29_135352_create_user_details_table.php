a33149960fc939920d38bae9171196aa96a9d91a
Eligio Biasi <ex_98@live.it>
2023-08-29 16:35:10 +0200

    created migration for user detail

---
+++ database/migrations/2023_08_29_135352_create_user_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->primary();
+            $table->string('user_name');
+            $table->string('user_description');
+            $table->string('user_code');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
